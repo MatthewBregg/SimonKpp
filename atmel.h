5083b8fd0ab9b16a80b2a250677a3233b9f90409
matthew bregg <matthewbregg@gmail.com>
2020-02-23 13:59:48 -0600

    Mosfet Manipulating functions

--- atmel.h
+++ atmel.h
@@ -1,7 +1,24 @@
 #ifndef ATMEL_H
 #define ATMEL_H
 
+constexpr byte T0CLK = 1U << CS01; // CLK/8 == 2 MHZ.
+constexpr byte T1CLK = 0xC1u;
+constexpr byte T2CLK = 1U << CS20; // (CLK/2) 16 MHZ
+constexpr byte TIMER_INTERRUPTS_ENABLE =  (1U<<TOIE1) | (1U<<OCIE1A) | (1U<<TOIE2);
+
+
+// We want to do this AFTER beeping!
+void enableTimerInterrupts() {
+    // https://web.ics.purdue.edu/~jricha14/Timer_Stuff/TIFR.htm
+    TIFR = TIMER_INTERRUPTS_ENABLE; // Clear TOIE1, OCIE1A, and TOIE2 flags
+    // https://web.ics.purdue.edu/~jricha14/Timer_Stuff/TIMSK.htm
+    TIMSK = TIMER_INTERRUPTS_ENABLE; // Enable t1ovfl_int, t1oca_int, t2ovfl_int
+}
+
 void setDefaultRegisterValues() {
+    ///////////////////////////////////////////////////
+    // Set the I/O registers to safe/default values. //
+    ///////////////////////////////////////////////////
     DDRB = 0x06u;
     PORTB = 0x06u;
 
@@ -10,6 +27,13 @@ void setDefaultRegisterValues() {
 
     DDRD = 0x3Eu;
     PORTD = 0x06u;
+
+    /////////////////////////////////////////////
+    // Set the TIMERS to their default values. //
+    /////////////////////////////////////////////
+    TCCR0 = T0CLK; // timer0: beep control, delays
+    TCCR1B = T1CLK; // timer1: communtation timing, RC pulse measurement.
+    TCCR2 = B00000000; // timer2: PWM, stopped.
 
 }
 
@@ -24,6 +48,61 @@ constexpr getByteWithBitCleared(byte bitIndex) {
 /***************************/
 /* Motor Driving Functions */
 /***************************/
+constexpr byte AnFetIdx = 3;
+constexpr byte BnFetIdx = 4;
+constexpr byte CnFetIdx = 5;
+constexpr byte ApFetIdx = 2;
+constexpr byte BpFetIdx = 2;
+constexpr byte CpFetIdx = 1;
+
+#define ANFET_PORT PORTD
+#define BNFET_PORT PORTD
+#define CNFET_PORT PORTD
+#define APFET_PORT PORTD
+#define BPFET_PORT PORTB
+#define CPFET_PORT PORTB
+
+// xNFets are active high.
+void AnFetOn() {
+    ANFET_PORT = ANFET_PORT | getByteWithBitSet(AnFetIdx);
+}
+void BnFetOn() {
+    BNFET_PORT = BNFET_PORT | getByteWithBitSet(BnFetIdx);
+}
+void CnFetOn() {
+    CNFET_PORT = CNFET_PORT | getByteWithBitSet(CnFetIdx);
+}
+
+void AnFetOff() {
+    ANFET_PORT = ANFET_PORT & getByteWithBitCleared(AnFetIdx);
+}
+void BnFetOff() {
+    BNFET_PORT = BNFET_PORT & getByteWithBitCleared(BnFetIdx);
+}
+void CnFetOff() {
+    CNFET_PORT = CNFET_PORT & getByteWithBitCleared(CnFetIdx);
+}
+
+// xPFets are active low.
+void ApFetOff() {
+    APFET_PORT = APFET_PORT | getByteWithBitSet(ApFetIdx);
+}
+void BpFetOff() {
+    BPFET_PORT = BPFET_PORT | getByteWithBitSet(BpFetIdx);
+}
+void CpFetOff() {
+    CPFET_PORT = CPFET_PORT | getByteWithBitSet(CpFetIdx);
+}
+
+void ApFetOp() {
+    APFET_PORT = APFET_PORT & getByteWithBitCleared(ApFetIdx);
+}
+void BpFetOp() {
+    BPFET_PORT = BPFET_PORT & getByteWithBitCleared(BpFetIdx);
+}
+void CpFetOp() {
+    CPFET_PORT = CPFET_PORT & getByteWithBitCleared(CpFetIdx);
+}
 
 
 /************************************************************/
