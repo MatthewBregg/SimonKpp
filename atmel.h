43166e03c8608107c7dc50e1583b297a5947141b
matthew bregg <matthewbregg@gmail.com>
2020-02-24 19:29:14 -0600

    Added interrupts

--- atmel.h
+++ atmel.h
@@ -2,7 +2,7 @@
 #define ATMEL_H
 
 // Notes: delayMicroseconds is a NOP loop, no interrupts!
-https://electronics.stackexchange.com/questions/84776/arduino-delaymicroseconds
+// https://electronics.stackexchange.com/questions/84776/arduino-delaymicroseconds
 // Millis and Micros() both use timer0, whose frequency we clobbered.
 // https://www.best-microcontroller-projects.com/arduino-millis.html
 // https://ucexperiment.wordpress.com/2012/03/17/examination-of-the-arduino-micros-function/
--- simonkpp.ino
+++ simonkpp.ino
@@ -25,3 +25,39 @@ void loop() {
     delay(2000);
     return;
 }
+
+volatile bool oct1_pending = false;
+volatile byte ocr1ax = 0; // third byte of OCR1A.
+volatile byte tcnt1x = 0; // third byte of TCNT1.
+volatile byte rc_timeout = 0;
+volatile byte rct_boot = 0; // Counter which increments while rc_timeout is 0 to jump to boot loader (TODO delete?)
+volatile byte rct_beacon = 0; // Counter which increments while rc_timeout is 0 to disarm and beep occasionally (TODO delete?)
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Interrupt Documentation							     //
+//    http://ee-classes.usc.edu/ee459/library/documents/avr_intr_vectors/	     //
+//    https://arduino.stackexchange.com/questions/32721/interrupts-on-the-arduino-ng //
+// BE VERY CAREFUL ABOUT USING RETURN STATEMENTS in interrupts, see                  //
+// https://www.avrfreaks.net/forum/return-statement-isr                              //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// timer1 output compare interrupt
+ISR(TIMER1_COMPA_VECT) {
+    if (ocr1ax < 1) {
+	oct1_pending = false; // Passed OCT1A.
+    }
+    --ocr1ax;
+}
+
+// timer1 overflow interrupt (happens every 4096µs)
+ISR(TIMER1_OVF_VECT) {
+    ++tcnt1x;
+
+    if ( tcnt1x & B1111 /* 15U */ == 0 ) {
+	if ( rc_timeout == 0 ) {
+	    // rc_timeout hit, increase the beacon.
+	    ++rct_boot;
+	    ++rct_beacon;
+	}
+    }
+}
